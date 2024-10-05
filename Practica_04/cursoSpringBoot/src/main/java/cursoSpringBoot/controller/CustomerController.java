f772e3693284792a2fa026fa46ec4d76d93a8595
Alfred <arista21dominguez@gmail.com>
2024-10-04 22:35:54 -0600

    Enrrutamiento de URL

--- Practica_04/cursoSpringBoot/src/main/java/cursoSpringBoot/controller/CustomerController.java
+++ Practica_04/cursoSpringBoot/src/main/java/cursoSpringBoot/controller/CustomerController.java
@@ -2,6 +2,7 @@ package cursoSpringBoot.controller;
 
 import cursoSpringBoot.domain.Customer;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -22,5 +23,23 @@ public class CustomerController
     public List<Customer> getCustomerList()
     {
         return customerList;
+    }
+
+    @GetMapping("/usuarios/{id}")
+    public Customer getCustomer(@PathVariable String id)
+    {
+        try
+        {
+            for (Customer c : customerList)
+            {
+                if (c.getId() == Integer.parseInt(id))
+                {
+                    return c;
+                }
+            }
+        }catch (Exception e)
+        {
+        }
+        return null;
     }
 }
