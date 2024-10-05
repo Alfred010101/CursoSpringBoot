0d32c632c9ae1aad04331f8e228c7ceaf8554b50
Alfred <arista21dominguez@gmail.com>
2024-10-04 22:14:32 -0600

    Pedir lista de usuarios

---
+++ Practica_04/cursoSpringBoot/src/main/java/cursoSpringBoot/controller/CustomerController.java
@@ -0,0 +1,26 @@
+package cursoSpringBoot.controller;
+
+import cursoSpringBoot.domain.Customer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class CustomerController
+{
+    private List<Customer> customerList = new ArrayList<>(Arrays.asList(
+            new Customer(111, "Ana", "anita", "********"),
+            new Customer(222, "Luis", "LUIS", "********"),
+            new Customer(333, "Pepe", "pepito", "********"),
+            new Customer(444, "Diana", "diana", "********")
+    ));
+
+    @GetMapping("/usuarios")
+    public List<Customer> getCustomerList()
+    {
+        return customerList;
+    }
+}
